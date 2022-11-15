701c178bf8c6b79bf8d91cb2af9cf18eb196bf19
JJBP55 <jonathanjbello.jb@gmail.com>
2022-11-14 22:38:15 -0400

    Class

--- main.js
+++ main.js
@@ -45,3 +45,61 @@ const juanita = new Student(
         "Curso de Creación de Personajes",
     ]
 );
+
+//Prototipos con las sintaxis de Clases
+
+class Student2 {
+    constructor(name, age, cursosAprobados){
+        this.name = name;
+        this.age = age;
+        this.cursosAprobados = cursosAprobados;
+    }
+
+    //metodos
+    aprobarCurso(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso);
+    }
+};
+
+//nueva instancia
+
+const miguel = new Student2(
+    "Miguel Cabrera",
+    28,
+    "Curso de Análisis de Negocios para ciencia de Datos",
+    "Curso de Principios de Visualización de Datos para BI"
+);
+
+
+class Student2 {
+    constructor({
+        name,
+        age,
+        twitter,
+        instagram,
+        facebook,
+        cursosAprobados = [],
+        email,
+    }){
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.instagram,
+        this.facebook,
+        this.twitter,
+        this.cursosAprobados = cursosAprobados;
+    }
+
+    //metodos
+    aprobarCurso(nuevoCurso){
+        this.cursosAprobados.push(nuevoCurso);
+    }
+};
+
+//nueva instancia
+
+const miguelito = new Student2({
+    email: "miguelito@platzi.com",
+    age: 28,
+    name: "Miguel"
+});
